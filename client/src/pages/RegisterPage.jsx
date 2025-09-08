403f5bd3911542d933ecbfb4537eddc45ec3fb28
Beat <beaterftr@gmail.com>
2025-09-08 15:39:37 +0700

    feat: implement user authentication with login and registration APIs

--- client/src/pages/RegisterPage.jsx
+++ client/src/pages/RegisterPage.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { useAuth } from "../contexts/authentication";
 
 function RegisterPage() {
   const [username, setUsername] = useState("");
@@ -6,10 +7,20 @@ function RegisterPage() {
   const [lastName, setLastName] = useState("");
   const [password, setPassword] = useState("");
 
+  // get register function from AuthContext
+  const { register } = useAuth();
+
   const handleSubmit = (event) => {
     event.preventDefault();
     // 🐨 Todo: Exercise #2
     // นำ Function `register` ใน AuthContext มา Execute ใน Event Handler ตรงนี้
+    const data = {
+      username,
+      password,
+      firstName,
+      lastName,
+    };
+    register(data);
   };
 
   return (
